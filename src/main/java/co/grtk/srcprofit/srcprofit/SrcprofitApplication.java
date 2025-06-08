30a002bd912c09b86f8c8b2c56eea0db18ae2e98
Imre <imre.tatai@gmail.com>
2025-06-08 11:02:36 +0200

    Initial commit

---
+++ src/main/java/co/grtk/srcprofit/srcprofit/SrcprofitApplication.java
@@ -0,0 +1,13 @@
+package co.grtk.srcprofit.srcprofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcprofitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrcprofitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/grtk/srcprofit/srcprofit/SrcprofitApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grtk.srcprofit.srcprofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcprofitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
